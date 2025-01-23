c54122d87f7e181747c6fb792f0e605e60671f82
thaneous <zaikov823@gmail.com>
2025-01-23 12:54:08 +0200

    Add contains.js to common folder for shared constants

---
+++ template/src/common/constants.js
@@ -0,0 +1,69 @@
+export const TRENDING = 'trending';
+
+export const FAVORITES = 'favorites';
+
+export const ABOUT = 'about';
+
+export const UPLOAD = 'upload';
+
+export const CONTAINER_SELECTOR = '#container';
+
+export const FULL_HEART = '❤';
+
+export const EMPTY_HEART = '♡';
+
+export const API_KEY = 'ZeiqAdGlCIR6UTnbrl1DqNZO412hIGHJ';
+
+/**
+ * A trending GIF endpoint
+ * @param {number} limit - the limit of gifs shown on the page
+ * @param {number} offset Specifies the starting position of the results.
+ * @return {string} The endpoint link
+ */
+export const getTrendingURL = (limit = 25, offset = 0) => `
+https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${limit}&offset=${offset}&rating=g`;
+
+/**
+ * A searched GIF endpoint
+ * @param {string} q - the text you are looking for
+ * @param {number} limit - the limit of gifs shown on the page
+ * @param {*} offset Specifies the starting position of the results.
+ * @return {string} The endpoint link
+ */
+export const getSearchGifs = (q='', limit = 20, offset = 0) => `
+https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${q}&limit=${limit}&offset=${offset}&rating=g`;
+
+
+/**
+ * A random GIF endpoint
+ * @return {string} The endpoint link
+ */
+export const getRandomGifs = () => `
+https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`;
+
+
+/**
+ * Uploads a GIF to Giphy
+ * @param {string} url - a URL or Base64 encoded string of the GIF
+ * @param {string} sourceUrl - a URL of the GIF on the user's website
+ * @param {string} tags - comma separated list of tags
+ * @return {string} The endpoint link
+ */
+export const getUploadedURL = (url = '', sourceUrl = '', tags) => `
+https://upload.giphy.com/v1/gifs?api_key=${API_KEY}&file=${url}&source_image_url=${sourceUrl}&tags=${tags}`;
+
+
+/**
+ * A GIF by ID endpoint
+ * @param {string} id - the ID of the GIF
+ * @return {string} The endpoint link
+ */
+export const getGifByID = (id) => `https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}&rating=g`;
+
+/**
+ * Uploaded by the user gifs by Ids endpoint
+ * @param  {...any} ids - A list of ids
+ * @return {string} The endpoint link
+ */
+export const uploadedIdsEndpoint = (...ids) => `
+https://upload.giphy.com/v1/gifs?api_key=${API_KEY}&ids=${ids}`;
