2cdd0a343fc86d75122763333c3309a1b0673822
Martin Mesechkov <mesechkov.martin@gmail.com>
2025-01-27 00:41:36 +0200

    Add favorite status rendering to trending view for GIFs

--- template/src/views/trending-view.js
+++ template/src/views/trending-view.js
@@ -1,3 +1,4 @@
+import { renderFavoriteStatus } from '../events/favorites-events.js';
 export const toTrendingView = (gifs) => `
 <div id="trending">
   <h1>Trendings</h1>
@@ -5,6 +6,7 @@ export const toTrendingView = (gifs) => `
     ${gifs.data.map(gif => `
       <div class="trending-item">
         <a href="#${gif.id}" id="details" data-gif-id="${gif.id}"><img src="${gif.images.downsized_large.url}" alt="${gif.title}" /></a>
+        ${renderFavoriteStatus(gif.id)}
         </div>
     `).join('')}
   </div>
