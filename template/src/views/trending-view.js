f0b6269675805d776fbaf7e3a155d19d2b1fe2dc
VikajaK1 <nikolay.kodzheykov.a67@learn.telerikacademy.com>
2025-01-26 18:54:54 +0200

    Enhance navigation layout and styling, improve search functionality, and update trending section design

--- template/src/views/trending-view.js
+++ template/src/views/trending-view.js
@@ -1,6 +1,9 @@
 export const toTrendingView = (gifs) => `
 <div id="trending">
-  <h1>Trendings</h1>
+<div class="section-info">
+<i class="fa-solid fa-arrow-trend-up fa-2xl" style="color: #FFD43B;"></i>
+  <h1 class='section-title'>Trending Now</h1>
+  </div>
   <div class="content">
     ${gifs.data.map(gif => `
       <div class="trending-item">
